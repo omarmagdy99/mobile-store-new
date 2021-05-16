6c61ea6ffb2d9de3dcb906f6647d81ddd75bb328
omarmagdy <omarmagdy99860@gmil.com>
2021-05-16 21:37:08 +0200

    omar

--- resources/views/pages/products/products.blade.php
+++ resources/views/pages/products/products.blade.php
@@ -1,4 +1,4 @@
-a@extends('layouts.master')
+@extends('layouts.master')
 @section('css')
     <!-- Internal Data table css -->
     <link href="{{ URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" />
